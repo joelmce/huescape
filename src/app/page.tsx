b70356fe1e30ad8c8fd8c0ef349ed20de1a4e737
joelmce <joelanthony.mac@gmail.com>
2023-12-11 23:25:40 +1100

    No title just yet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import GradientCanvas from "../../components/GradientCanvas";
 export default function Home() {
   return (
     <>
-      <h1 className="text-lg text-bold">Huescape.</h1>
       <div className="flex items-center justify-center h-screen">
         
         <div className="flex items-center justify-center">
