a1e9e233c09c33eec4eadcefc9b2e48550a6c38b
joelmce <joelanthony.mac@gmail.com>
2023-12-11 22:15:17 +1100

    Add gradient factory

---
+++ components/GradientCanvas.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { useState } from "react"
+import { generateMeshGradient } from "../util/ColorFactory"
+import { Copy, RefreshCw, Check } from "react-feather";
+
+const ELEMENTS = 6
+
+export default function GradientCanvas() {
+    const [history, setHistory] = useState([generateMeshGradient(ELEMENTS)])
+    const [index, setIndex] = useState(0)
+    const [copied, setCopied] = useState(false)
+
+    const handleNewGradient = () => {
+        setIndex(history.length)
+        setHistory([...history, generateMeshGradient(ELEMENTS)])
+    }
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText("")
+        setCopied(true)
+        setTimeout(() => {
+            setCopied(false)
+        }, 3000)
+    }
+
+    return (
+        <>
+            <div className="flex flex-col gap-5">
+                <div
+                    style={history[index]}
+                    className="z-10 w-96 h-96 rounded-xl"
+                />
+                <button
+                    onMouseDownCapture={() => handleNewGradient()}
+                    className='z-10 transition text-tertiary hover:text-sky-500 text-white text-sm bg-black w-fit py-2 px-2 rounded-xl'
+                >
+                    <RefreshCw size={15} className="inline"/> Generate
+                </button>
+
+                {copied ? <Check/> : <Copy onClick={handleCopy} className="hover:cursor-pointer hover:text-green-500"/>}
+            </div>
+        
+            <div
+                className='absolute w-[300px] h-[500px] md:w-[800px] md:h-[700px] mt-64 opacity-[16%] backdrop-blur-3xl blur-3xl pointer-events-none rounded-[15rem]'
+                style={history[index]}
+            />
+            
+        </>
+    )
+}
---
+++ util/ColorFactory.ts
@@ -0,0 +1,110 @@
+const randomColour = (): number => {
+    return Math.round(Math.random() * 360)
+}
+
+const getPercentage = (v: number): number => {
+    return Math.round((Math.random() * (v * 100)) % 100);
+}
+
+// Credit: https://gist.github.com/xenozauros/f6e185c8de2a04cdfecf
+const hexToHSL = (hex?: string): number | undefined => {
+    if (!hex) return undefined;
+    hex = hex.replace(/#/g, "");
+    if (hex.length === 3) {
+      hex = hex
+        .split("")
+        .map(function (hex) {
+          return hex + hex;
+        })
+        .join("");
+    }
+    var result = /^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})[\da-z]{0,0}$/i.exec(hex);
+    if (!result) {
+      return undefined;
+    }
+    var r = parseInt(result[1], 16);
+    var g = parseInt(result[2], 16);
+    var b = parseInt(result[3], 16);
+    (r /= 255), (g /= 255), (b /= 255);
+    var max = Math.max(r, g, b),
+      min = Math.min(r, g, b);
+    var h = (max + min) / 2;
+    if (max == min) {
+      h = 0;
+    } else {
+      var d = max - min;
+      switch (max) {
+        case r:
+          h = (g - b) / d + (g < b ? 6 : 0);
+          break;
+        case g:
+          h = (b - r) / d + 2;
+          break;
+        case b:
+          h = (r - g) / d + 4;
+          break;
+      }
+      h /= 6;
+    }
+    h = Math.round(360 * h);
+  
+    return h;
+  };
+
+const getHashPercentage = (
+    v: number,
+    hash: number,
+    length: number
+): number => {
+    return Math.round(((hash / length) * (v * 100)) % 100);
+}
+
+const generateColours = (length: number, initialHue: number): string[] => {
+    return Array.from({length}, (_, i) => {
+        if (i === 0) {
+            return `hsl(${initialHue}, 100%, 74%)`
+        }
+
+        if(i < length / 1.4) {
+            return `hsl(${
+                initialHue - 30 * (1 - 2 * (i % 2)) * (i > 2 ? i / 2 : i)
+            }, 100%, ${64 - i * (1 - 2 * (i % 2)) * 1.75}%)`;
+        }
+
+        return `hsl(${initialHue - 150 * (1 - 2 * (i % 2))}, 100%, ${
+            66 - i * (1 - 2 * (i % 2)) * 1.25
+        }%)`; 
+    })
+}
+
+const generateGradient = (length: number, colours: string[], hash?: number) => {
+    return Array.from({ length}, (_, i) => {
+        return `radial-gradient(at ${
+            hash ? getHashPercentage(i, hash, length) : getPercentage(i)
+        }% ${hash ? getHashPercentage(i * 10, hash, length) : getPercentage(i * 10)}%, ${
+            colours[i]
+        } 0px, transparent 55%)\n`
+    })
+}
+
+const generateStops = (length: number, base?: number, hash?: number) => {
+    const colours = generateColours(length, base ? base : randomColour())
+
+    const properties = generateGradient(length, colours, hash ? hash : undefined)
+    return [colours[0], properties.join(",")]
+}
+
+const generateMeshGradient = (
+    length: number,
+    base?: string,
+    hash?: number
+) => {
+    const [bgColour, bgImage] = generateStops(
+        length, hexToHSL(base) ? hexToHSL(base) : undefined,
+        hash ? hash : undefined
+    )
+
+    return { backgroundColor: bgColour, backgroundImage: bgImage };
+}
+
+export { generateMeshGradient as generateMeshGradient}
