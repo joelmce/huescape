1cc7fc4aff8442b3ee755a9a36b1dd71be217660
joelmce <joelanthony.mac@gmail.com>
2023-12-13 23:21:46 +1100

    Fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Huescape',
-  description: 'Just generates some clean mesh gradients.',
+  description: 'A simple gradient generator that whips up 6 random colour stops which you can then copy & paste into your CSS.',
 }
 
 export default function RootLayout({
@@ -16,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   )
 }
