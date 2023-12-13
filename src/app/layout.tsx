9b5626fdf8dc0594d1c334ee572f43a9d2f682db
joelmce <joelanthony.mac@gmail.com>
2023-12-13 23:40:57 +1100

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <Analytics/>
       </body>
     </html>
   )
